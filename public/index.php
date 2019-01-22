85935908181bf1b6ea7c5e490505a527772d7387
Ben Russell <benrussell@brussell-ml.lan>
2019-01-22 01:23:34 -0700

    site skeleton coming together

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/head.php'; ?>
+    <body>
+        <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/header.php'; ?>
+        <main>
+            <div class="container text-center my-5 py-5">
+                <p>[ Homepage Coming Soon ]</p>
+            </div>
+            <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/footer.php'; ?>
+        </main>
+        <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/close-page.php'; ?>
+    </body>
+</html>
---
+++ public/partials/header.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-white text-black">
+        <a class="navbar-brand" href="/">
+            <img class="tmtrails-logo" src="/assets/images/tmtrails-logo.svg" alt="tmtrails logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/trails">Trails</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/donate">Donate</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact-us">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
