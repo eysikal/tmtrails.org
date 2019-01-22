94d942e8caacd29a395e995177d3426b60b359cc
Ben Russell <benrussell@brussell-ml.lan>
2019-01-22 14:06:08 -0700

    added trails and about pages. increased margins in header and footer

--- public/about/index.php
+++ public/about/index.php
@@ -4,8 +4,12 @@
     <body>
         <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/header.php'; ?>
         <main>
-            <div class="container text-center my-5 py-5">
-                <p>[ About Page Coming Soon ]</p>
+            <div class="container text-center">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <p class="mb-0">Our organization exists to promote development of local, sustainable trails for outdoor recreation (e.g., biking, hiking, etc.). Our organization is run and operated entirely by volunteers. We fundraise (grants and private donations) for the development and construction of new trails. We also work with community partners to ensure trails are used appropriately.</p>
+                    </div>
+                </div>
             </div>
             <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/footer.php'; ?>
         </main>
--- public/trails/index.php
+++ public/trails/index.php
@@ -4,8 +4,14 @@
     <body>
         <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/header.php'; ?>
         <main>
-            <div class="container text-center my-5 py-5">
-                <p>[ Trail Map Coming Soon ]</p>
+            <div class="container text-center">
+                <h1>Proposed Trail Map</h1>
+                <div class="embed-responsive embed-responsive-16by9 mt-4">
+                    <iframe class="embed-responsive-item"
+                            src="http://docs.google.com/gview?url=https://www.traversemountaintrails.org/assets/files/tmtrails-proposed-map.pdf&embedded=true"
+                            allowfullscreen></iframe>
+                </div>
+                <p class="mt-4"><a href="/assets/files/tmtrails-map.pdf">Download Proposed Trail Map</a></p>
             </div>
             <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/footer.php'; ?>
         </main>
