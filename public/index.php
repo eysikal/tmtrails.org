dc211d16c97f0f5b808b8cc31518d2b932d6ed2d
Ben Russell <benrussell@brussell-ml.lan>
2019-01-22 14:35:59 -0700

    added carousel to homepage. turned jquery on because it’s a dependency for the carousel. added images for carousel. killed example bootstrap files that shouldn’t have been there in the first place.

--- public/index.php
+++ public/index.php
@@ -4,8 +4,31 @@
     <body>
         <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/header.php'; ?>
         <main>
-            <div class="container text-center my-5 py-5">
-                <p>[ Homepage Coming Soon ]</p>
+            <div class="container text-center">
+                <h1>Traverse Mountain Trails Association</h1>
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div id="tmtrails-carousel" class="carousel slide mt-4" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="/assets/images/tmtrails-landscape-1.jpg" class="d-block w-100" alt="Traverse Mountain Landscape 1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/assets/images/tmtrails-landscape-2.jpg" class="d-block w-100" alt="Traverse Mountain Landscape 2">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/assets/images/tmtrails-landscape-3.jpg" class="d-block w-100" alt="Traverse Mountain Landscape 3">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#tmtrails-carousel" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                            </a>
+                            <a class="carousel-control-next" href="#tmtrails-carousel" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
             <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/footer.php'; ?>
         </main>
