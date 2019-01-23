7d9880ba857446175f9d54f27b0adddf77a0a707
Ben Russell <benrussell@brussell-ml.lan>
2019-01-22 19:16:15 -0700

    ripped out PDF embedding nonsense and replaced with an image. Users can download the full PDF if they want.

--- public/trails/index.php
+++ public/trails/index.php
@@ -5,13 +5,9 @@
         <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/header.php'; ?>
         <main>
             <div class="container text-center">
-                <h1>Proposed Trail Map</h1>
-                <div class="embed-responsive embed-responsive-16by9 mt-4">
-                    <iframe class="embed-responsive-item"
-                            src="https://docs.google.com/gview?url=https://www.traversemountaintrails.org/assets/files/tmtrails-proposed-map.pdf&embedded=true"
-                            allowfullscreen></iframe>
-                </div>
-                <p class="mt-4"><a href="/assets/files/tmtrails-map.pdf">Download Proposed Trail Map</a></p>
+                <h1 class="mb-4">Proposed Trail Map</h1>
+                <img class="img-fluid" src="/assets/images/tmtrails-proposed-map.jpg" alt="tmtrails proposed map">
+                <p class="mt-4"><a href="/assets/files/tmtrails-proposed-map.pdf">Download Full-Resolution Map</a></p>
             </div>
             <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/footer.php'; ?>
         </main>
