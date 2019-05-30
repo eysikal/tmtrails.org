caf0a736849fbc4b0c3eca649a06244bca8808e6
Benjamin Russell <eysikal@gmail.com>
2019-05-29 22:06:35 -0600

    deleted old trail plan map and added approved maps

--- public/trails/index.php
+++ public/trails/index.php
@@ -5,9 +5,13 @@
         <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/header.php'; ?>
         <main>
             <section class="container text-center">
-                <h1 class="mb-5">Proposed Trail Map</h1>
-                <img class="img-fluid" src="/assets/images/tmtrails-proposed-map.jpg" alt="tmtrails proposed map">
-                <p class="mt-4"><a href="/assets/files/tmtrails-proposed-map.pdf">Download Full-Resolution Map</a></p>
+                <h1 class="mb-5">Trail Maps</h1>
+                <h2>Approved Master Plan</h2>
+                <img class="img-fluid" src="/assets/images/master-plan-a-web.jpg" alt="tmtrails proposed map">
+                <p class="mt-4"><a href="/assets/files/tmtrails-master-plan.pdf" target="_blank">Download Full-Resolution Map</a></p>
+                <h2 class="mt-5">Trail Construction Priority</h2>
+                <img class="img-fluid" src="/assets/images/master-plan-a-web.jpg" alt="tmtrails proposed map">
+                <p class="mt-4"><a href="/assets/files/tmtrails-construction-priority.pdf" target="_blank">Download Full-Resolution Map</a></p>
             </section>
         </main>
         <?php include $_SERVER["DOCUMENT_ROOT"] . '/partials/footer.php'; ?>
